1e8a4484c5d26fe582025bdade068b0ad6156c92
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-04-02 15:14:08 -0700

    Add styling to PhotoListItem image

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,16 +4,26 @@ const PhotoListItem = ({ sampleDataForPhotoListItem }) => {
   const { urls, user, location } = sampleDataForPhotoListItem;
 
   return (
-    <section>
-      <img src={urls.regular} alt={urls.regular} />
-      <img src={user.profile} alt={`user${urls.username}`} />
-      <p>{user.username}</p>
-      <p>
-        <span>{location.city}</span>
-        {location.country}
-        <span></span>
-      </p>
-    </section>
+    <div className="photo-list__item">
+      <img
+        src={urls.regular}
+        alt={urls.regular}
+        className="photo-list__image"
+      />
+      <div className="photo-list__user-details">
+        <img
+          src={user.profile}
+          alt={`user${urls.username}`}
+          className="photo-list__user-profile "
+        />
+        <p>{user.username}</p>
+        <p>
+          <span>{location.city}</span>
+          {location.country}
+          <span></span>
+        </p>
+      </div>
+    </div>
   );
 };
 
