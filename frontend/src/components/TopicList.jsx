0fdd3eb931679a3678b2aaa46dbf08c0d1983132
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-04-03 14:24:09 -0700

    Refactor PhotoList and TopicList tp use mock data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,26 +1,9 @@
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
-
-const sampleDataForTopicList = [
-  {
-    id: 1,
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: 2,
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: 3,
-    slug: "topic-3",
-    title: "People",
-  },
-];
+import topics from "../mocks/topics";
 
 const TopicList = () => {
-  const parsedTopics = sampleDataForTopicList.map((topic) => (
+  const parsedTopics = topics.map((topic) => (
     <TopicListItem key={topic.id} topic={topic} />
   ));
 
