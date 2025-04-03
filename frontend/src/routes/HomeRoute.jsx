404223b8787fecfa88b881528e159c9ee6dae7f7
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-04-03 14:01:00 -0700

    Implement HomeRoute.jsx

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,9 +1,12 @@
-import '../styles/HomeRoute.scss';
+import TopNavigation from "../components/TopNavigationBar";
+import PhotoList from "../components/PhotoList";
+import "../styles/HomeRoute.scss";
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigation />
+      <PhotoList />
     </div>
   );
 };
