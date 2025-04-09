faab49973c15048b97b1d13bef90fef9cdf0f57a
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-04-08 20:58:35 -0700

    Refactor PhotoDetailsModal to reuse PhotoList component

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,7 +1,8 @@
 import "../styles/PhotoDetailsModal.scss";
 import closeSymbol from "../assets/closeSymbol.svg";
 import MainPhotoModal from "../components/MainPhotoModal";
-import SimilarPhotoList from "../components/SimilarPhotoList";
+// import SimilarPhotoList from "../components/SimilarPhotoList";
+import PhotoList from "../components/PhotoList";
 
 const PhotoDetailsModal = ({
   selectedPhoto,
@@ -12,6 +13,8 @@ const PhotoDetailsModal = ({
   const handleButtonClick = () => {
     closeModal();
   };
+
+  const photos = Object.values(selectedPhoto.similar_photos); //added this line to pass the same param
 
   return (
     <>
@@ -34,8 +37,8 @@ const PhotoDetailsModal = ({
           <p>Similar Photos</p>
         </header>
 
-        <SimilarPhotoList
-          similarPhotosObject={selectedPhoto.similar_photos}
+        <PhotoList
+          photos={photos} //modified this to pass to PhotoList
           favoritePhotos={favoritePhotos}
           updateToFavPhotoIds={updateToFavPhotoIds}
         />
